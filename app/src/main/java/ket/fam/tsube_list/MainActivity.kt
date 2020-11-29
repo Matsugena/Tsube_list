0be488d781d14222000c798e2dd0d909f01c1ce5
M keita <norimaki.kent@gmail.com>
2020-11-29 10:46:08 +0900

    YoutubeStandalonePlayer の実行まで

--- app/src/main/java/ket/fam/tsube_list/MainActivity.kt
+++ app/src/main/java/ket/fam/tsube_list/MainActivity.kt
@@ -1,12 +1,15 @@
 package ket.fam.tsube_list
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.youtube.player.YouTubeStandalonePlayer
+import ket.fam.tsube_list.resource.YOUTUBE_SAMPLE_URL
+import ket.fam.tsube_list.resource.YoutubeAPIKey
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +21,18 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val intent = YouTubeStandalonePlayer.createVideoIntent(
+                this, YoutubeAPIKey, YOUTUBE_SAMPLE_URL
+        )
+        startActivity(intent)
     }
+
 }
